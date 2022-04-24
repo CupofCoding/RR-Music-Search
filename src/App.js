c7ef8e529116baa8db6c2e100cd0762084b99bbe
Anne <anne@cupofcoding.com>
2022-04-24 14:11:12 -0700

    updated per assignment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import {useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from './components/Gallery'
 import Searchbar from './components/Searchbar'
 import { DataContext } from './context/DataContext'
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
+import { Fragment } from 'react/cjs/react.production.min'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -32,12 +36,31 @@ function App() {
 	}
 
 	return (
-		<div>
+		/*<div>
 			<Searchbar handleSearch = {handleSearch}/>
 			{message}
 			<DataContext.Provider value={data}>
 				<Gallery />
 			</DataContext.Provider>
+		</div>*/
+
+		<div>
+			{message}
+			<Router>
+				<Routes>
+					<Route 
+						path="/"
+						element={
+							<Fragment>
+								<Searchbar handleSearch = {handleSearch}/>
+								<Gallery data={data} />
+							</Fragment>
+						}
+					/>
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
   	);
 }
